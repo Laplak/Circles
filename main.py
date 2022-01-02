8d69697865c7c0cbf0284fbbd334183dcbb8e4eb
Илья Крутилин <Laplak99@mail.ru>
2022-01-02 23:13:42 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+import random
+
+from UI import WindowSample
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
+
+class Circles(QWidget, WindowSample):
+    def __init__(self):
+        super().__init__()
+        self.circle = QLabel(self)
+
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.makeCircles)
+
+    def makeCircles(self):
+        y = random.choice(range(650))
+        x = random.choice(range(850))
+
+        side = random.choice(range(650))
+
+        self.circle.setGeometry(x, y, side, side)
+        self.circle.setStyleSheet(f'border-radius: {side // 2}; background-color: #FFFF00;')
+        self.circle.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Circles()
+    ex.show()
+    sys.exit(app.exec_())
